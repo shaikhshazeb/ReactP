30c3ace62238d2dee7cbafc02f9897d2fd6db3a5
shaikh_shazeb <shazebs077@gmail.com>
2024-11-03 18:30:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Home from './Componenets/Home';
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Componenets/Home.js
@@ -0,0 +1,233 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <><header>
+      <div className='Head'>
+        <br/>
+      <div id='logo'>
+     <img src='image.svg'></img>
+      </div>
+      <div id='nav'>
+        <ul>
+      <li>
+        <a href='#'>Brand Partner</a>
+        </li>
+        <li>
+        <a href='#'>BeutyPeneur</a>
+        </li>
+        <li>
+        <a href='#'>Fasilitas</a>
+        </li>
+        </ul>
+      </div>
+      <div id='btn1'>
+      <button>Registrasi</button>
+      </div>
+      </div>
+      <div id='fake'></div>
+    </header>
+   
+ <section>
+  <div className='hero'>
+    <br/>
+    <div id='img2'>
+    <img src='lull.svg'></img>
+    </div>
+  <div id='heading'>
+ <h1>Pasti berat</h1>
+ <h2>kalo lari sendirian</h2>
+ <p>Setiap hal pasti butuh hal lain untuk membersamai.
+Begitupun dengan bisnis, Beautieslfie.id hadir untuk membersamai
+kalian untuk menjadi Beautypreneur. Siapapun kamu!!</p>
+  </div>
+  &nbsp;
+  <div id='para'>&nbsp;
+<h3>1st Indonesian Beautypreneur Community</h3>
+<h4>Dengan berbagai fasilitas “advance disigital platform”.</h4>
+  </div>
+  <div id='Himg'>
+  <img src='heroi.svg'></img>
+  </div>
+  </div>
+  
+ </section>
+ <section>
+  <div className='Hero2'>
+    <br/>
+<div id='head3'>
+
+</div>
+<div id='ni'>
+
+</div>
+<div id='img5'>
+<img src='img5.svg'></img>
+</div>
+<div id='bg'>
+<img src='bg.svg'></img>
+</div>
+<div id='hr'>
+<h1>
+Beautypreneur.
+</h1>
+</div>
+<div id='pr'>
+  <p>Bukan sekedar bisnis yang hits dengan segudang untung.
+  Tren beautypreneur di Indonesia sudah mulai terlihat  sejak beberapa tahun terakhir, industri kecantikan dalam negeri maulai berkembang pesat. </p>
+</div>
+  </div>
+ </section>
+ <section>
+  <div className='hero4'>
+    <br/>
+    
+  <div id='hd1'>
+<h1>Beauty Selling <span>with</span>&nbsp;Digital Pattern.</h1>
+</div>
+<div id='hdp'>
+<p>Pasar yang besar ditunjang dengan wadah yang tepat, insyaAllah akan bebuah keberhasilan</p>
+</div>
+<div id='head6'>
+  
+<div id='md1'>
+  
+  </div>
+  <div id='md2'>
+  <h1>Bukan saatnya menunda <span>digitalisasi.</span> </h1>
+  </div>
+  <br/><br/>
+  <div id='mdp'>
+  <p>
+  Ayo kita hadapi bersama era peralihan ini. Beautieslife.id akan bantu kamu belajar berbisnis dengan berbagai tools digital yang diharapkan akan mendorong kemudahan dan kelancaran bisnis beauty kamu.
+  </p>
+  </div>
+  <br/><br/>
+  <div id='mdc'>
+    <br/>
+    <div id='mdc1'><img src='dot.svg'></img><p>Udah bukan jamannya gaptek</p></div>
+    <div id='mdc2'><img src='dot.svg'></img><p>Udah gak ada lagi bingung pake tools apaan</p></div>
+    <div id='mdc3'><img src='dot.svg'></img><p>Gak ada lagi istilah siapa yang bakan ngajarin aku</p></div>
+    <div id='mdc4'><img src='dot.svg'></img><p>Udah bukan musimnya jalan sendirian!!</p></div>
+  </div>
+</div>
+
+<div id='bg2'>
+<img src='gimg.svg'></img>
+</div>
+<div id='ig1'>
+<img src='ig1.svg'></img>
+</div>
+<div id='ig2'>
+<img src='ig2.svg'></img>
+</div>
+<div id='ig3'>
+<img src='ig3.svg'></img>
+</div>
+<div id='ig4'>
+<img src='ig4.svg'></img>
+</div>
+<div id='gimg'>
+<img src='Group.svg'></img>
+</div>
+  </div>
+ </section>
+ <section>
+  <div className='hero5'>
+    <br/>
+    <div id='mon'></div>
+<div id='hn1'>
+<h1>Pokonya udah pasti kita bimbing,<span>dengan berbagai fasilitas.</span> </h1>
+</div>
+<br/>
+<div id='container'>
+  <br/>
+<div id='c1'>
+<img src='c1.svg'></img>
+</div>
+<div id='c2'>
+<img src='c2.svg'></img>
+</div>
+<div id='c3'>
+<img src='c3.svg'></img>
+</div>
+<div id='c4'>
+<img src='c4.svg'></img>
+</div>
+<div id='c5'>
+<img src='c5.svg'></img>
+</div>
+<div id='c6'>
+<img src='c6.svg'></img>
+</div>
+<div id='c7'>
+<img src='c7.svg'></img>
+</div>
+<div id='c8'>
+<img src='c8.svg'></img>
+</div>
+</div>
+<div id='bgg'>
+<img src='bbg.svg'></img>
+</div>
+<div id='null'>
+<img src='bgg.svg'></img>
+</div>
+  </div>
+  <br/><br/><br/><br/><br/>
+ </section>
+ <section>
+  <div className='last'>
+    <br/><br/><br/>
+<div id='jk'>
+<h1>Meet our<span> mentor</span> and <span>advisor.</span></h1>
+</div>
+<div id='po'>
+<p>Kita akan berlari ditemani para mentor dan advisor dari Beautieslife.id. Akan banyak ilmu baru dan inspiratif.</p>
+</div>
+<div id='cont'>
+  
+  <div id='con1'>
+<img src='con1.svg'></img>
+  </div>
+  <div id='con2'>
+  <img src='con2.svg'></img>
+  </div>
+  <div id='con3'>
+  <img src='con3.svg'></img>  
+  </div>
+  <div id='con4'>
+  <img src='con4.svg'></img>  
+  </div>
+</div>
+<div id='g'>
+  <img src='g.svg'></img>
+</div>
+  </div>
+  <br/>
+ </section>
+ <footer>
+  <div className='foot'>
+    <br/> <br/> <br/>
+    <div id='none'>
+      <img src='none.svg'></img>
+    </div>
+    <div id='lasto'>
+      <br/>  <br/>
+      <div id='popo'>
+        <img src='popo.svg'>
+        </img>
+      </div>
+    </div>
+    <div id='Frame'>
+      <img src='Frame.svg'></img>
+    </div>
+  
+  </div>
+  <br/>
+ </footer>
+    </>
+  )
+}
+
+export default Home
